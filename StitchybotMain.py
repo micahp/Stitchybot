02ee749d60ee850326222279c6c8099e0ee4e0cf
Weston <weston@WestonUbuntu.(none)>
2014-05-04 18:55:02 -0500

    1.0? Sure why not

--- StitchybotMain.py
+++ StitchybotMain.py
@@ -26,70 +26,95 @@ class Category:
 	def __init__(self, name, key):
 		self.name = name
 		self.key = key
-		self.score = 0
-		self.retweets = 0
+		self.individualScore = 0
+		self.retweetsTotal = 0
+		self.retweetsList = []
 
 	def incrementRetweets(self, num):
 		print("Adding " + str(num) + " retweets to category " + self.name)
-		self.retweets = self.retweets + num
+		self.retweetsTotal = self.retweetsTotal + num
+		self.retweetsList.append(num)
+
+	def calculateScore(self):
+		if len(self.retweetsList) > 3:
+			bonusTweetCount = len(self.retweetsList) / 4
+		else:
+			bonusTweetCount = 1
+		bonus = 3
+		temp = 0
+		for score in reversed(self.retweetsList):
+			temp += score*bonus
+			bonus -= (bonus - 1)/bonusTweetCount
+		self.individualScore = temp
+
 
 class Categories:
 	def __init__(self):
 		self.container = []
+		self.total = 0
 
 	def addCategory(self, c):
 		self.container.append(c)
-
-	def listOfKeys(self):
-		result = []
-		for category in self.container:
-			result.append(category.key)
-		return result
 
 	def getCategory(self, key):
 		for category in self.container:
 			if(category.key == key):
 				return category
+		return None
 
 def save_object(obj, filename):
     with open(filename, 'wb') as output:
         pickle.dump(obj, output, pickle.HIGHEST_PROTOCOL)
 
-twitterAuth()
 
+def loadData():
 #load previous data if available, else create a new data file
-try:
-	with open('data.pkl', 'rb') as input:
-		print "Loading previous file..."
-		categories = pickle.load(input)
-except:
-	print("Creating new file...")
-	categories = Categories()
-	f = open('categories.txt', 'r')
-	lines = open('categories.txt').read().splitlines()
-	for line in lines:
-		c = Category(line, line[0])
-		categories.addCategory(c)
-	save_object(categories, r'data.pkl')
+	global categories
+	try:
+		with open('data.pkl', 'rb') as input:
+			print "Loading previous data..."
+			categories = pickle.load(input)
+	except:
+		print("Creating new data...")
+		categories = Categories()
+		f = open('categories.txt', 'r')
+		lines = open('categories.txt').read().splitlines()
+		for line in lines:
+			categories.addCategory(Category(line, line[0]))
+		save_object(categories, r'data.pkl')
 
-try:
-	with open('cache.pkl', 'rb') as input:
-		print("Cache found, loading...")
-		cache = pickle.load(input)
-except:
-	print("Creating new cache...")
-	cache = []
-	save_object(cache, r'cache.pkl')
+	global cache	
+	try:
+		with open('cache.pkl', 'rb') as input:
+			print("Cache found, loading...")
+			cache = pickle.load(input)
+	except:
+		print("Creating new cache...")
+		cache = []
+		save_object(cache, r'cache.pkl')
 
 
+twitterAuth()
+loadData()
+
+print("Incremeting category retweets...")
 tweets = api.user_timeline()
 for tweet in tweets:
 	if tweet.id not in cache:
 	    categoryTag = tweet.text[0]
-	    if categoryTag not in categories.listOfKeys():
-	    	print("Invalid category, skipping tweet")
+	    currentCategory = categories.getCategory(categoryTag)
+	    if currentCategory != None:
+	    	currentCategory.incrementRetweets(tweet.retweet_count)
+	    	categories.total += tweet.retweet_count
+	    	currentCategory.calculateScore()
 	    else:
-	    	categories.getCategory(categoryTag).incrementRetweets(tweet.retweet_count)
+	    	print("Invalid category, skipping tweet")
 	    cache.append(tweet.id)
+
+print("Recalculating scores...")
+for category in categories.container:
+	print(category.name + " Score: " + str(category.individualScore))
+
+
 save_object(cache, r'cache.pkl')
 save_object(categories, r'data.pkl')
